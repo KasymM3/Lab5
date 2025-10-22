ede9c5803d6b3bda0d6a8c361c6cccec39e5c140
KasymM3 <gaisin.k26@gmail.com>
2025-10-22 20:40:45 +0500

    Push

--- src/main/java/kz/lab5/lab5/Repository/OperatorsRepository.java
+++ src/main/java/kz/lab5/lab5/Repository/OperatorsRepository.java
@@ -3,6 +3,8 @@ package kz.lab5.lab5.Repository;
 
 import kz.lab5.lab5.Entity.Operators;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface OperatorsRepository extends JpaRepository<Operators,Long> {
 }
